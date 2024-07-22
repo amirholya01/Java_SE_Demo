fbe9ca10e4a53e24aa089a05138788b2e0a5ace3
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-07-22 13:50:29 +0200

    done it

--- PhoneBook_App_Demo/src/dk/amir/PhoneBook.java
+++ PhoneBook_App_Demo/src/dk/amir/PhoneBook.java
@@ -1,10 +1,13 @@
 package dk.amir;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PhoneBook {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> numbers = new ArrayList<>();
         int choice ;
         do {
             System.out.println("---Menu---");
@@ -15,10 +18,21 @@ public class PhoneBook {
             choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.println("Add contact");
+                    System.out.println("Enter contact name: ");
+                    String name = scanner.next();
+                    System.out.println("Enter contact number: ");
+                    String number = scanner.next();
+                    names.add(name);
+                    numbers.add(number);
                     break;
                 case 2:
-                    System.out.println("Print all contacts");
+                     if (names.isEmpty() || numbers.isEmpty()) {
+                         System.out.println("You have not entered any contacts!");
+                     }else {
+                         for (int i = 0; i < names.size(); i++) {
+                             System.out.println(names.get(i) + " " + numbers.get(i));
+                         }
+                     }
                     break;
                 case 3:
                     System.exit(0);
