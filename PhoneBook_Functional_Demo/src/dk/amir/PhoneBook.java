fbbd67b131f3bc1198cb0f19f932fb8162998b32
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-07-23 08:54:54 +0200

    print menu

---
+++ PhoneBook_Functional_Demo/src/dk/amir/PhoneBook.java
@@ -0,0 +1,13 @@
+package dk.amir;
+
+public class PhoneBook {
+    public static void main(String[] args) {
+        printMenu();
+    }
+    public static void printMenu(){
+        System.out.println("----Menu----");
+        System.out.println("1 - Add Phone");
+        System.out.println("2 - Display All Phones");
+        System.out.println("3 - Exit\n");
+    }
+}
