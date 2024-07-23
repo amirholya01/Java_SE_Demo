b2d9e4dfaaac59d14edd2ea3cd18442b4043d8e0
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-07-23 09:21:34 +0200

    completed

--- PhoneBook_Functional_Demo/src/dk/amir/PhoneBook.java
+++ PhoneBook_Functional_Demo/src/dk/amir/PhoneBook.java
@@ -1,20 +1,63 @@
 package dk.amir;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PhoneBook {
+    private static ArrayList<String> names = new ArrayList<>();
+    private static ArrayList<String> numbers = new ArrayList<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        printMenu();
-        System.out.println("Enter your choice:");
-        int choice = scanner.nextInt();
-        System.out.println(choice);
+        int choice;
+        //System.out.println(choice);
+        do {
+            printMenu();
+            System.out.println("Enter your choice:");
+             choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1:
+                    addContact();
+                    break;
+                case 2:
+                    displayAll();
+                    break;
+                case 3:
+                    System.out.println("Exit!");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+        }while (choice != 3);
         scanner.close();
     }
-    public static void printMenu(){
+
+    private static void printMenu(){
         System.out.println("----Menu----");
         System.out.println("1 - Add Phone");
         System.out.println("2 - Display All Phones");
         System.out.println("3 - Exit\n");
+    }
+
+    private static void addContact(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter name:");
+        String name = scanner.next();
+        System.out.println("Enter number:");
+        String number = scanner.next();
+        names.add(name);
+        numbers.add(number);
+        scanner.close();
+    }
+
+    private static void displayAll(){
+        if(names.isEmpty() || numbers.isEmpty()){
+            System.out.println("No names or numbers");
+        }else {
+            for (int i = 0; i < names.size(); i++) {
+                System.out.println(i+1 + " - " + names.get(i) + " - " + numbers.get(i));
+            }
+        }
     }
 }
