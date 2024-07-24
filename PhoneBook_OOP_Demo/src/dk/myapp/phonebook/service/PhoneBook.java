008ea0d75b9f3e1b31cbd818893ca410f5796a21
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-07-24 07:39:39 +0200

    sample business

--- PhoneBook_OOP_Demo/src/dk/myapp/phonebook/service/PhoneBook.java
+++ PhoneBook_OOP_Demo/src/dk/myapp/phonebook/service/PhoneBook.java
@@ -1,4 +1,38 @@
 package dk.myapp.phonebook.service;
 
+import dk.myapp.phonebook.model.Contact;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PhoneBook {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            menu();
+            System.out.println("Please enter your choice");
+            choice = scanner.nextInt();
+            switch (choice){
+                case 1:
+                    System.out.println("Add");
+                    break;
+                case 2:
+                    System.out.println("Print");
+                    break;
+                case 3:
+                    System.out.println("Exit");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+        }while (choice != 3);
+    }
+    public static void menu(){
+        System.out.println("---Welcome to Phone Book---");
+        System.out.println("1. Add Contact");
+        System.out.println("2. Print All Contacts");
+        System.out.println("3. Exit\n");
+    }
 }
