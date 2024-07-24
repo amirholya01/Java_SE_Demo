7ff63e2862e9fe58d94986bc21ccbe0605a012ac
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-07-24 12:50:17 +0200

    showAllContacts

--- PhoneBook_OOP_CRUD/src/dk/amir/phonebook/service/PhoneBook.java
+++ PhoneBook_OOP_CRUD/src/dk/amir/phonebook/service/PhoneBook.java
@@ -1,8 +1,47 @@
 package dk.amir.phonebook.service;
 
+import dk.amir.phonebook.model.Contact;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PhoneBook {
+    private ArrayList<Contact> contacts = new ArrayList<>();
     public void run(){
-        printMenu();
+       Scanner scanner = new Scanner(System.in);
+       int choice ;
+       boolean flag = true;
+       while (flag){
+           printMenu();
+           String BLUE = "\u001B[34m";
+           String RESET = "\u001B[0m";
+           System.out.println(BLUE + "Enter your choice:" + RESET);
+           choice = scanner.nextInt();
+           switch (choice){
+               case 1:
+                   System.out.println("add contact");
+                   break;
+               case 2:
+                   printAllContacts();
+                   break;
+               case 3:
+                   System.out.println("edit contact");
+                   break;
+               case 4:
+                   System.out.println("delete contact");
+                   break;
+               case 5:
+                   System.out.println("search contact");
+                   break;
+               case 6:
+                   flag = false;
+                   break;
+               default:
+                   System.out.println("Invalid choice");
+                   break;
+           }
+       }
+       scanner.close();
     }
 
     private void printMenu(){
@@ -16,4 +55,16 @@ public class PhoneBook {
         System.out.println("5. Search Contact");
         System.out.println("6. Exit\n");
     }
+
+    private void printAllContacts(){
+        if (contacts.isEmpty())
+            System.out.println("No contacts found");
+        else{
+            for (Contact contact : contacts){
+                System.out.println(contact);
+            }
+        }
+
+    }
+
 }
