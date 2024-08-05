8dbd3933310cfa535d4c4bfcf3fca63239b00bb7
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-08-05 10:59:30 +0200

    customer class and customerType-enum

---
+++ Customer-System/src/dk/customerSystem/enums/CustomerType.java
@@ -0,0 +1,6 @@
+package dk.customerSystem.enums;
+
+public enum CustomerType {
+    LEGAL_CUSTOMER,
+    REAL_CUSTOMER
+}
---
+++ Customer-System/src/dk/customerSystem/model/Customer.java
@@ -0,0 +1,77 @@
+package dk.customerSystem.model;
+
+import dk.customerSystem.enums.CustomerType;
+
+public abstract class Customer {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private CustomerType customerType;
+
+    public Customer(String firstName, String lastName, String phoneNumber, String email, CustomerType customerType) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.customerType = customerType;
+    }
+
+    private String capitalizedFirstLetter(String str) {
+        if(!str.isEmpty()){
+            return Character.toUpperCase(str.charAt(0)) + str.substring(1);
+        }
+        return "Not Valid";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = capitalizedFirstLetter(firstName);
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = capitalizedFirstLetter(lastName);
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public CustomerType getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(CustomerType customerType) {
+        this.customerType = customerType;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", customerType=" + customerType +
+                '}';
+    }
+}
