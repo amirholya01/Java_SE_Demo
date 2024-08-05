2d2c42c37bb841ae72c5e162cb2c489462b82d34
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-08-05 11:19:25 +0200

    printMenu

--- Customer-System/src/dk/customerSystem/service/CustomerSystem.java
+++ Customer-System/src/dk/customerSystem/service/CustomerSystem.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 public class CustomerSystem implements AutoCloseable{
     Scanner scanner = new Scanner(System.in);
     public void run(){
-        System.out.println("Welcome to Customer System");
+        printMenu();
+    }
+
+    private void printMenu(){
+        System.out.println("-------------------------------------------");
+        System.out.println("Please choose one of the following options:");
+        System.out.println("0. Exit");
+        System.out.println("1. Create Customer");
     }
     @Override
     public void close()  {
