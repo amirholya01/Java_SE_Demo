b822c24c45c987f88384eccb96fbda5d16c049ca
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-08-05 11:52:19 +0200

    Basic definition of addNewCustomer method

--- Customer-System/src/dk/customerSystem/service/CustomerSystem.java
+++ Customer-System/src/dk/customerSystem/service/CustomerSystem.java
@@ -16,12 +16,28 @@ public class CustomerSystem implements AutoCloseable{
                     System.out.println("Exit Application !!!!");
                     break;
                 case 1:
-                    System.out.println("Add Customer");
+                    addNewCustomer();
                     break;
                 default:
                     System.out.println("Invalid choice");
             }
         }while (choice != 0);
+    }
+
+    private void addNewCustomer() {
+        System.out.println("Please choose one of the following options:");
+        System.out.println("1 - Real Customer:");
+        System.out.println("2 - Legal Customer:");
+        System.out.println("Please enter your choice: ");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        if (choice == 1) {
+            System.out.println("Real Customer");
+        }else if (choice == 2) {
+            System.out.println("Legal Customer");
+        }else
+            System.out.println("Invalid choice");
+
     }
 
     private void printMenu(){
